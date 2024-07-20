4d564acc224c79103b177b4fa0a1a8452adb6129
Agustin Espana <agustinespania@gmail.com>
2024-07-20 10:11:39 +0200

    feat: added randomization and test cases

--- go/helpers/internal/client/retry.go
+++ go/helpers/internal/client/retry.go
@@ -1,42 +1,76 @@
 package client
 
-type Backoff interface {
-	SetInitialInterval(interval int)
-	SetMultiplier(multiplier float32)
-	SetRandomizationFactor(factor float32)
-	Retry(req func())
-}
+import (
+	"math/rand"
+	"time"
+)
 
-type ExponentialBackoff struct {
+type Operation[T any] func() (T, error)
+
+type BackoffRetry struct {
 	InitialInterval     int32
 	Multiplier          float32
 	RandomizationFactor float32
+	MaxRetries          int32
 }
 
-func NewExponentialBackoffDefault() *ExponentialBackoff {
-	return &ExponentialBackoff{
+func NewBackoffRetryDefault() *BackoffRetry {
+	return &BackoffRetry{
 		InitialInterval:     500,
 		Multiplier:          1.5,
 		RandomizationFactor: 0.5,
+		MaxRetries:          10,
 	}
 }
 
-func NewExponentialBackoff(initialInterval int32, multiplier float32, randomizationFactor float32) *ExponentialBackoff {
-	return &ExponentialBackoff{
+func NewBackoffRetry(initialInterval int32, multiplier float32, randomizationFactor float32, maxRetries int32) *BackoffRetry {
+	return &BackoffRetry{
 		InitialInterval:     initialInterval,
 		Multiplier:          multiplier,
 		RandomizationFactor: randomizationFactor,
+		MaxRetries:          maxRetries,
 	}
 }
 
-func (b *ExponentialBackoff) SetInitialInterval(interval int32) {
+func (b *BackoffRetry) SetInitialInterval(interval int32) {
 	b.InitialInterval = interval
 }
 
-func (b *ExponentialBackoff) SetMultiplier(multiplier float32) {
+func (b *BackoffRetry) SetMultiplier(multiplier float32) {
 	b.Multiplier = multiplier
 }
 
-func (b *ExponentialBackoff) SetRandomizationFactor(factor float32) {
+func (b *BackoffRetry) SetRandomizationFactor(factor float32) {
 	b.RandomizationFactor = factor
+}
+
+func (b *BackoffRetry) getRandomizedInterval(interval float32) float32 {
+	delta := b.RandomizationFactor * interval
+	minInterval := interval - delta
+	maxInterval := interval + delta
+
+	randomFactor := rand.Float32()
+	return minInterval + (maxInterval-minInterval)*randomFactor
+}
+
+func Retry[T any](o Operation[T], b BackoffRetry) (T, error) {
+	interval := b.InitialInterval
+	var res T
+	var err error
+	var i int32 = 0
+
+	for i <= b.MaxRetries {
+		res, err = o()
+		if err == nil {
+			return res, nil
+		}
+
+		randomizedInterval := b.getRandomizedInterval(float32(interval))
+		time.Sleep(time.Duration(randomizedInterval) * time.Millisecond)
+
+		interval = int32(b.Multiplier * float32(interval))
+		i++
+	}
+
+	return res, err
 }
--- go/helpers/internal/client/retry_test.go
+++ go/helpers/internal/client/retry_test.go
@@ -1,9 +1,13 @@
 package client
 
-import "testing"
+import (
+	"errors"
+	"testing"
+	"time"
+)
 
 func TestNewExponentialBackoffDefault(t *testing.T) {
-	eb := NewExponentialBackoffDefault()
+	eb := NewBackoffRetryDefault()
 
 	if eb.InitialInterval != 500 {
 		t.Errorf("Expected InitialInterval to be 500, got %d", eb.InitialInterval)
@@ -24,15 +28,16 @@ func TestNewExponentialBackoff(t *testing.T) {
 		initialInterval     int32
 		multiplier          float32
 		randomizationFactor float32
+		maxRetries          int32
 	}{
-		{"Custom Values", 1000, 2.0, 0.3},
-		{"Zero Values", 0, 0, 0},
-		{"Mixed Values", 750, 1.75, 0},
+		{"Custom Values", 1000, 2.0, 0.3, 6000},
+		{"Zero Values", 0, 0, 0, 0},
+		{"Mixed Values", 750, 1.75, 0, 6000},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			eb := NewExponentialBackoff(tt.initialInterval, tt.multiplier, tt.randomizationFactor)
+			eb := NewBackoffRetry(tt.initialInterval, tt.multiplier, tt.randomizationFactor, tt.maxRetries)
 
 			if eb.InitialInterval != tt.initialInterval {
 				t.Errorf("Expected InitialInterval to be %d, got %d", tt.initialInterval, eb.InitialInterval)
@@ -49,26 +54,162 @@ func TestNewExponentialBackoff(t *testing.T) {
 	}
 }
 
-// func TestMain(m *testing.M) {
-// 	bo := backoff.NewExponentialBackOff()
+func TestGetRandomizedInterval(t *testing.T) {
+	backoff := BackoffRetry{
+		RandomizationFactor: 0.5,
+	}
 
-// 	err := backoff.Retry(func() error {
-// 		resp, err := http.Get("https://jsonplaceholder.typicode.com/todos/1")
-// 		if err != nil {
-// 			return err
-// 		}
-// 		defer resp.Body.Close()
+	interval := float32(1000)
+	for i := 0; i < 100; i++ {
+		randomizedInterval := backoff.getRandomizedInterval(interval)
 
-// 		var result map[string]interface{}
-// 		if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
-// 			return err
-// 		}
+		delta := backoff.RandomizationFactor * interval
+		minInterval := interval - delta
+		maxInterval := interval + delta
 
-// 		fmt.Printf("%+v\n", result)
-// 		return nil
-// 	}, bo)
+		if randomizedInterval < minInterval || randomizedInterval > maxInterval {
+			t.Errorf("Randomized interval %f out of expected range [%f, %f]", randomizedInterval, minInterval, maxInterval)
+		}
+	}
+}
 
-//		if err != nil {
-//			fmt.Println("Request failed:", err)
-//		}
-// }
+func TestRetry(t *testing.T) {
+	backoff := BackoffRetry{
+		MaxRetries:          8,
+		InitialInterval:     500,
+		RandomizationFactor: 0.5,
+		Multiplier:          1.5,
+	}
+
+	expectedTimeRange := func(initial int32, multiplier float32, retries int32) (min, max time.Duration) {
+		min, max = 0*time.Millisecond, 0*time.Millisecond
+		interval := float32(initial)
+		var i int32
+		for i = 0; i <= retries; i++ {
+			delta := interval * backoff.RandomizationFactor
+			min += time.Duration(interval-delta) * time.Millisecond
+			max += time.Duration(interval+delta) * time.Millisecond
+			interval = interval * multiplier
+		}
+		return
+	}
+
+	// Test case 1: Operation succeeds on first try
+	t.Run("SuccessOnFirstTry", func(t *testing.T) {
+		count := 0
+		op := func() (interface{}, error) {
+			count++
+			return 42, nil
+		}
+
+		start := time.Now()
+		result, err := Retry(op, backoff)
+		end := time.Now()
+		elapsed := end.Sub(start)
+
+		if err != nil {
+			t.Errorf("Expected no error, got %v", err)
+		}
+		if result != 42 {
+			t.Errorf("Expected result 42, got %d", result)
+		}
+		if count != 1 {
+			t.Errorf("Expected operation to be called once, was called %d times", count)
+		}
+
+		expectedMaxDuration := time.Duration(backoff.InitialInterval) * time.Millisecond
+		if elapsed >= expectedMaxDuration {
+			t.Errorf("Elapsed time %v surpassed expected max of %d ns", elapsed, expectedMaxDuration)
+		}
+	})
+
+	// Test case 2: Operation succeeds after a few retries
+	t.Run("SuccessAfterFewRetries", func(t *testing.T) {
+		count := 0
+		op := func() (interface{}, error) {
+			count++
+			if count < 3 {
+				return nil, errors.New("temporary error")
+			}
+			return "success", nil
+		}
+
+		start := time.Now()
+		result, err := Retry(op, backoff)
+		end := time.Now()
+		elapsed := end.Sub(start)
+
+		if err != nil {
+			t.Errorf("Expected no error, got %v", err)
+		}
+		if result != "success" {
+			t.Errorf("Expected result 'success', got '%s'", result)
+		}
+		if count != 3 {
+			t.Errorf("Expected operation to be called 3 times, was called %d times", count)
+		}
+
+		min, max := expectedTimeRange(backoff.InitialInterval, backoff.Multiplier, int32(count-1))
+		if elapsed < min || elapsed > max {
+			t.Errorf("Elapsed time %v out of expected range [%v, %v]", elapsed, min, max)
+		}
+	})
+
+	// Test case 2: Operation succeeds after some retries
+	t.Run("SuccessAfterSomeRetries", func(t *testing.T) {
+		count := 0
+		op := func() (interface{}, error) {
+			count++
+			if count < 8 {
+				return nil, errors.New("temporary error")
+			}
+			return "success", nil
+		}
+
+		start := time.Now()
+		result, err := Retry(op, backoff)
+		end := time.Now()
+		elapsed := end.Sub(start)
+
+		if err != nil {
+			t.Errorf("Expected no error, got %v", err)
+		}
+		if result != "success" {
+			t.Errorf("Expected result 'success', got '%s'", result)
+		}
+		if count != 8 {
+			t.Errorf("Expected operation to be called 8 times, was called %d times", count)
+		}
+
+		min, max := expectedTimeRange(backoff.InitialInterval, backoff.Multiplier, int32(count-1))
+		if elapsed < min || elapsed > max {
+			t.Errorf("Elapsed time %v out of expected range [%v, %v]", elapsed, min, max)
+		}
+	})
+
+	// Test case 3: Operation never succeeds
+	t.Run("NeverSucceeds", func(t *testing.T) {
+		count := 0
+		op := func() (interface{}, error) {
+			count++
+			return nil, errors.New("persistent error")
+		}
+
+		start := time.Now()
+		_, err := Retry(op, backoff)
+		end := time.Now()
+		elapsed := end.Sub(start)
+
+		if err == nil {
+			t.Error("Expected an error, got nil")
+		}
+		if count != int(backoff.MaxRetries+1) {
+			t.Errorf("Expected operation to be called %d times, was called %d times", backoff.MaxRetries+1, count)
+		}
+
+		min, max := expectedTimeRange(backoff.InitialInterval, backoff.Multiplier, backoff.MaxRetries)
+		if elapsed < min || elapsed > max {
+			t.Errorf("Elapsed time %v out of expected range [%v, %v]", elapsed, min, max)
+		}
+	})
+}
